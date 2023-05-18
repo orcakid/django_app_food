06b4f8b7d4507962265720ac5ce7e584490b93b4
orcakid <orcakid97@gmail.com>
2023-05-18 21:44:33 +0300

    users again

--- users/models.py
+++ users/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-from django.contrib.auth.base_user import AbstractBaseUser
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email address', unique=True)
+    password = models.CharField(verbose_name="password", max_length=150)
 
-
-
-class User(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    nickname = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username','first_name','last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+    
+    def __str__(self):
+        return self.username
